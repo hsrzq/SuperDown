a338056a4a28ea8bbf2f17fe159bb8c321ef52cf
zhangqi <me@zhangqi.space>
2017-06-03 00:49:31 +0800

    Add ordered/unordered list support

--- src/cn/hsrzq/SuperDown.php
+++ src/cn/hsrzq/SuperDown.php
@@ -163,6 +163,26 @@ class SuperDown
                     $blocks[++$position] = ['toc', $key, $key];
                     break;
                 }
+                // ordered list
+                case preg_match('/^[0-9a-z]+\..+/', $line, $matches): {
+                    // already ol
+                    if ($blocks[$position] && $blocks[$position][0] == 'ol') {
+                        $blocks[$position][2] = $key;
+                    } else {
+                        $blocks[++$position] = ['ol', $key, $key];
+                    }
+                    break;
+                }
+                // unordered list
+                case preg_match('/^\+.+/', $line, $matches): {
+                    // already ul
+                    if ($blocks[$position] && $blocks[$position][0] == 'ul') {
+                        $blocks[$position][2] = $key;
+                    } else {
+                        $blocks[++$position] = ['ul', $key, $key];
+                    }
+                    break;
+                }
                 default: {
                     $blocks[++$position] = ['normal', $key, $key];
                     break;
@@ -216,6 +236,34 @@ class SuperDown
             $headings = $result;
         }
         return $this->makeTocEntry($headings);
+    }
+
+    private function makeOl(array $lines, array $block)
+    {
+        list($type, $start, $end, $extra) = $block;
+        $lines = array_slice($lines, $start, $end - $start + 1);
+
+        $blocks = $this->parseList($lines, '/^([0-9a-z]+\.)(.+)$/');
+        $html = "<ol>";
+        foreach ($blocks as $block) {
+            $html .= "<li>" . $this->parseLines(array_slice($lines, $block[0], $block[1] - $block[0] + 1)) . "</li>";
+        }
+        $html .= "</ol>";
+        return $html;
+    }
+
+    private function makeUl(array $lines, array $block)
+    {
+        list($type, $start, $end, $extra) = $block;
+        $lines = array_slice($lines, $start, $end - $start + 1);
+
+        $blocks = $this->parseList($lines, '/^(\+)(.+)$/');
+        $html = "<ul>";
+        foreach ($blocks as $block) {
+            $html .= "<li>" . $this->parseLines(array_slice($lines, $block[0], $block[1] - $block[0] + 1)) . "</li>";
+        }
+        $html .= "</ul>";
+        return $html;
     }
 
     private function makeNormal(array $lines, $block)
@@ -308,5 +356,21 @@ class SuperDown
         }
         $html .= "</ul>";
         return $html;
+    }
+
+    private function parseList(&$lines, $pattern)
+    {
+        $position = -1;
+        $blocks = [];
+        foreach ($lines as $key => $line) {
+            if (preg_match($pattern, $line, $matches)) {
+                $blocks[++$position] = [$key, $key, $matches];
+                $lines[$key] = trim($matches[2]);
+            } else {
+                $blocks[$position][1] = $key;
+            }
+        }
+
+        return $blocks;
     }
 }
